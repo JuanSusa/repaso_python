4f0a0338d7cb4982e88d4bb7d0ec1c49f04c85ff
JuanSusa <kmilosusasusa@gmail.com>
2023-05-04 20:28:31 -0500

    termiancion repaso estructuras condicionales

---
+++ repaso_python/ejerRepaso.py
@@ -0,0 +1,14 @@
+"""
+# Declaracion variables
+idStudent = 1013676004
+nameStudent = "Juan"
+lastNameStudent = "Susa"
+stateStudent = True
+
+# Impresion
+print("El estudiante idenficado con " ,idStudent, ", se llama ", nameStudent , " y esta ", stateStudent , " en el sistema ")
+"""
+# num1 = int(input("Digite el primer numero \n"))
+# num2 = int(input("Digite el segundo numero \n"))
+# total = num1 + num2
+# print("La suma entre el primer numero " , num1 , " y " , num2 , " es de: " , total)
